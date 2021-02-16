98e9ec1edd88a601735ca860691608080beb9161
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-02-16 22:53:51 +0530

    use of escape sequences on string

--- src/com/practiceQuestion/Practice.java
+++ src/com/practiceQuestion/Practice.java
@@ -16,8 +16,7 @@ public static void  main(String []args){
     c = c*a;
     System.out.println("Updated value of c:"+c);
      */
-    /*
-    3. use of primitive datatypes
+    /*3. use of primitive datatypes
     int a = 100;
     long b = 12345678;
     long l = a*b;
@@ -26,7 +25,15 @@ public static void  main(String []args){
     double area=2*pi*f;
     System.out.println("value of l is :"+l);
     System.out.println("Area is:"+area);
-
      */
+
+    System.out.println("'welcome'");
+    System.out.println("\"welcome\"");
+    System.out.println("wel\ncome");
+    System.out.println("'wel\\come'");
+    System.out.println("'wel\rcome'");
+    System.out.println("'wel\fcome'");
+    System.out.println("'wel\tcome'");
+    System.out.println("'wel\bcome'");
 }
 }
