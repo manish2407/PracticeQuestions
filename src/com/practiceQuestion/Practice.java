8df439cd7223b413c4b36748c89573099c663ac2
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-02-16 22:14:12 +0530

    use of primitive data types

--- src/com/practiceQuestion/Practice.java
+++ src/com/practiceQuestion/Practice.java
@@ -8,11 +8,25 @@ public static void  main(String []args){
     /* 1.Basic program
     System.out.println("Hello,welcome to the java world");
      */
+
+    /*2. use of identifiers and literals
     int a=10,b=20;
     int c=a+b;
     System.out.print("value of c:"+c);
     c = c*a;
     System.out.println("Updated value of c:"+c);
+     */
+    /*
+    3. use of primitive datatypes
+    int a = 100;
+    long b = 12345678;
+    long l = a*b;
+    double f = 10.4;
+     double pi = 3.1412;
+    double area=2*pi*f;
+    System.out.println("value of l is :"+l);
+    System.out.println("Area is:"+area);
 
+     */
 }
 }
