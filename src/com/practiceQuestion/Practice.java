45e100f5e3e045a24641d1f6903b3d3317a0cdd2
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-02-16 21:58:46 +0530

    program using identifiers and literals

--- src/com/practiceQuestion/Practice.java
+++ src/com/practiceQuestion/Practice.java
@@ -5,6 +5,14 @@ package com.practiceQuestion;
 
 public class Practice {
 public static void  main(String []args){
+    /* 1.Basic program
     System.out.println("Hello,welcome to the java world");
+     */
+    int a=10,b=20;
+    int c=a+b;
+    System.out.print("value of c:"+c);
+    c = c*a;
+    System.out.println("Updated value of c:"+c);
+
 }
 }
