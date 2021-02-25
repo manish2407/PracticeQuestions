e3ed11939fddc4e899f59d59d21e60b47e9739bd
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-02-26 00:18:31 +0530

    use of real class and creation of instance and object

--- src/com/practiceQuestion/BoxDemo.java
+++ src/com/practiceQuestion/BoxDemo.java
@@ -1,13 +1,24 @@
 package com.practiceQuestion;
 
 public class BoxDemo {
+    int length;
+    int breadth;
+    int height;
+
     public static void main(String[] args) {
         Box my = new Box();
-        double volume;
+        BoxDemo my1 = new BoxDemo();
+        double volume1;
+        double volume2;
         my.l = 10;
         my.b = 10;
         my.h = 10;
-        volume = my.l*my.b*my.h;
-        System.out.println("volume is :" + volume);
+        my1.length = 20;
+        my1.breadth = 20;
+        my1.height = 20;
+        volume1 = my.l*my.b*my.h;
+        volume2 = my1.length*my1.breadth*my1.height;
+        System.out.println("volume of Box class is :" + volume1);
+        System.out.println("volume of BoxDemo class is :" + volume2);
     }
 }
