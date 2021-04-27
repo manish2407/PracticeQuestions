e9099e97ef9451cc9e2024425be02fab7f9b1c0a
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-04-27 13:12:36 +0530

    BankApp class for execution

---
+++ src/RunnableBankProgram/BankApp.java
@@ -0,0 +1,8 @@
+package RunnableBankProgram;
+
+public class BankApp {
+    public static void main(String[] args) {
+        BankAccount b1 = new BankAccount(100.0);
+        System.out.println("before transation");
+    }
+}
