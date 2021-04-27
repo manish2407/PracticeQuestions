2a3cd494c5a43248b944a8dad62505587a96f2a5
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-04-27 13:13:36 +0530

    balance after deposit

--- src/RunnableBankProgram/BankApp.java
+++ src/RunnableBankProgram/BankApp.java
@@ -4,5 +4,8 @@ public class BankApp {
     public static void main(String[] args) {
         BankAccount b1 = new BankAccount(100.0);
         System.out.println("before transation");
+        b1.display();
+        b1.deposit(50.0);
+        System.out.println("After deposit");
     }
 }
