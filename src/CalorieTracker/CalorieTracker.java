c441340420fb28691cb12a3db7befe4ad0d7979f
manish2407 <manish.sharma_cs18@gla.ac.in>
2022-02-22 21:10:04 +0530

    WeightLostInAMonth Function

--- src/CalorieTracker/CalorieTracker.java
+++ src/CalorieTracker/CalorieTracker.java
@@ -1,4 +1,8 @@
 package CalorieTracker;
 
 public class CalorieTracker {
+    public float weightLostInAMonth(int cycling, int running, int swimming, int calorieIntake){
+}
+    public static void main(String args[]){
+    }
 }
