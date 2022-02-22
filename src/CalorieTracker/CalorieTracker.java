d43761dacf349420cec4887c46aa8bdc24dc75b6
manish2407 <manish.sharma_cs18@gla.ac.in>
2022-02-22 21:19:47 +0530

    Calorie Tracker Program

--- src/CalorieTracker/CalorieTracker.java
+++ src/CalorieTracker/CalorieTracker.java
@@ -2,7 +2,18 @@ package CalorieTracker;
 
 public class CalorieTracker {
     public float weightLostInAMonth(int cycling, int running, int swimming, int calorieIntake){
+        int calorieBurnt = cycling+running+swimming;
+        int totalCalorie = calorieBurnt-calorieIntake;
+        int calorieInMonth = totalCalorie*4;
+        int extraCalorie = 30*100;
+        int totalCalorieInAMonth = calorieInMonth+extraCalorie;
+        if(totalCalorieInAMonth>0){
+            return (float) (totalCalorieInAMonth/1000);
+        }
+        return -1;
 }
-    public static void main(String args[]){
+    public static void main(String[] args){
+        CalorieTracker ct = new CalorieTracker();
+        System.out.println("Weight Lost By Anne in a month is: "+ct.weightLostInAMonth(200,200,200,100)+" Pound");
     }
 }
